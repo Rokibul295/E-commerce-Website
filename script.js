7e7a0943aa36215efa014e6978764dc5dd67f52b
Rashadat12 <150833632+Rashadat12@users.noreply.github.com>
2025-12-21 21:43:48 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-import { StoreModel } from "./model/storeModel.js";
-import { StoreView } from "./view/storeView.js";
-import { StoreController } from "./controller/storeController.js";
-
-const model = new StoreModel();
-const view = new StoreView();
-new StoreController(model, view);
-
